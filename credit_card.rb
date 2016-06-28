30a80fd62757cf0c4e4c2781710413d338371cf5
Jason Noble <github+jasonn@jasonnoble.org>
2016-06-27 20:55:24 -0600

    Added CreditCard class

---
+++ credit_card.rb
@@ -0,0 +1,11 @@
+# This is my handy CreditCard class
+class CreditCard
+  attr_reader :balance
+  def initialize
+    @balance = 0.0
+  end
+
+  def charge(amount)
+    @balance += amount
+  end
+end
